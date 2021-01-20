a762c999421ae37356c960477d5b9c048fe94982
Cristian Laynez <54615310+CRLB-sketch@users.noreply.github.com>
2021-01-19 17:38:02 -0800

    Creación del proyecto

---
+++ app/src/main/java/com/example/appproyecto/MainActivity.kt
@@ -0,0 +1,26 @@
+/**
+ * <h1> Proyecto APP - Guate-Covidianos </h1>
+ * <h2> Main Activity </h2>
+ *
+ * Esta parte será donde se programar la pestaña principal
+ *
+ * <p>Desarrollo de Plataformas Moviles - Universidad del Valle de Guatemala </p>
+ *
+ * Creado por:
+ * @author [Cristian Laynez, Elean Rivas]
+ * @version 1.0
+ * @since 2020-Enero-19
+ *
+ **/
+
+package com.example.appproyecto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
