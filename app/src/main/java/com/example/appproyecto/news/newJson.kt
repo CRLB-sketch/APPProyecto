f32178517cbfb5066e21bad781152fa040af4b31
GitElean <eleanjulian@gmail.com>
2021-03-12 16:12:28 -0600

    Implementando clases para las noticias

---
+++ app/src/main/java/com/example/appproyecto/news/newJson.kt
@@ -0,0 +1,3 @@
+package com.example.appproyecto.news
+
+class newJson : ArrayList<newJsonItem>()
---
+++ app/src/main/java/com/example/appproyecto/news/newJsonItem.kt
@@ -0,0 +1,13 @@
+package com.example.appproyecto.news
+
+data class newJsonItem(
+    val active: Int,
+    val category_id: Int,
+    val detail: String,
+    val end: String,
+    val id: Int,
+    val image: String,
+    val name: String,
+    val start: String,
+    val title: String
+)
