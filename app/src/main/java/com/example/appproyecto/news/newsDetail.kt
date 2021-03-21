06b7eed4a5ebd3731df032c1baeb48da5c435bdf
GitElean <eleanjulian@gmail.com>
2021-03-21 17:06:58 -0600

    News detail xml

---
+++ app/src/main/java/com/example/appproyecto/news/newsDetail.kt
@@ -0,0 +1,12 @@
+package com.example.appproyecto.news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appproyecto.R
+
+class newsDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_detail)
+    }
+}
